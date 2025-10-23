2febc6e1bfb44d3be95a63961da9b278dde95a08
Sondre Bakken <countach1992@gmail.com>
2025-10-23 16:08:47 +0200

    removed sidebar

--- vite-project/src/App.tsx
+++ vite-project/src/App.tsx
@@ -1,14 +1,10 @@
 import "./App.css";
-import Sidebar from "./components/Sidebar";
 import ItemForm from "./components/ItemForm";
 import ItemList from "./components/ItemList";
 
 function App() {
   return (
     <>
-      <div className="App">
-        <Sidebar />
-      </div>
       <div>
         <h1>Item Management</h1>
         <ItemForm />
